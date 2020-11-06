1abb1addf85acab85a71639b51ebcc4c27c32c74
mkafonso <makaya2afonso@gmail.com>
2020-11-06 01:59:01 -0300

    setup and layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
+import React, { useState } from 'react';
+
+// import components
+import Navbar from './components/Navbar';
+import Planets from './components/Planets';
+import People from './components/People';
+
 function App() {
-  return <h1>Hello world!</h1>;
+  const [page, setPage] = useState('planets');
+
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>
+
+      <Navbar setPage={setPage} />
+
+      <div className="content">
+        {page === 'planets' ? <Planets /> : <People />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
